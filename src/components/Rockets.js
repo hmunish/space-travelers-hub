1a84cf5839b2b17b1d1ec2f77d9d2f2cd5c15ca1
Munish <hmunish5@gmail.com>
2023-07-24 16:12:54 +0200

    Add asyncThunk & extra reducers for fetching rockets data from API

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRockets } from '../redux/rockets/rocketsSlice';
 
-const Rockets = () => (
-  <div>Rockets</div>
-);
+const Rockets = () => {
+  const rocketsData = useSelector((state) => state.rocketsList.value);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, [dispatch]);
+
+  return (
+    <ul>
+      {rocketsData.map((rocket) => (
+        <li key={rocket.id}>{rocket.cost_per_launch}</li>
+      ))}
+    </ul>
+  );
+};
 
 export default Rockets;
--- src/redux/rockets/rocketsSlice.js
+++ src/redux/rockets/rocketsSlice.js
@@ -1,13 +1,43 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+export const fetchRockets = createAsyncThunk(
+  'rockets/fetchRockets',
+  async (thunkAPI) => {
+    try {
+      const res = await fetch('https://api.spacexdata.com/v4/rockets');
+      if (!res.ok) throw new Error('Unable to get data for the rockets');
+      const data = res.json();
+      return data;
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err.message);
+    }
+  },
+);
 
 const initialState = {
+  loading: false,
   value: [],
+  errors: null,
 };
 
 export const rocketsSlice = createSlice({
-  name: 'books',
+  name: 'rockets',
   initialState,
   reducers: {},
+
+  extraReducers: (builder) => {
+    builder.addCase(fetchRockets.fulfilled, (state, action) => {
+      state.loading = false;
+      state.value = action.payload;
+    });
+    builder.addCase(fetchRockets.rejected, (state, action) => {
+      state.loading = false;
+      state.errors = action.error.message;
+    });
+    builder.addCase(fetchRockets.pending, (state) => {
+      state.loading = true;
+    });
+  },
 });
 
 // Action creators are generated for each case reducer function
