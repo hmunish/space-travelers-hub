9de3739243ebfde32696ba9ac1a046c36ac3c241
Cosmas Wasswa <cosywas@gmail.com>
2023-07-24 16:07:50 +0300

    Setup for fetching the missions items with necessary tools

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchMissions } from '../redux/missions/missionSlice';
 
-const Missions = () => (
-  <div>Missions</div>
-);
+const Missions = () => {
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    dispatch(fetchMissions());
+  }, [dispatch]);
+
+  return (
+    <table>
+      <thead>
+        <tr className="head">
+          <th>Mission</th>
+          <th>Description</th>
+          <th>Status</th>
+          <th>&nbsp;</th>
+        </tr>
+      </thead>
+      <tbody className="t-body">
+        <tr>
+          <td>
+            <span>data</span>
+          </td>
+          <span>Not a Member</span>
+          <td>
+            <span>Data</span>
+          </td>
+          <td>
+            <span>Active Member</span>
+          </td>
+          <td>
+            <button type="button">Join Mission</button>
+          </td>
+        </tr>
+
+      </tbody>
+    </table>
+  );
+};
 export default Missions;
