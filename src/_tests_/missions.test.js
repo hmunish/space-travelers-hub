5abdf936369c22172e0d0dc110823e0559545378
Cosmas Wasswa <cosywas@gmail.com>
2023-07-25 14:02:25 +0300

    Fix linters

--- src/_tests_/missions.test.js
+++ src/_tests_/missions.test.js
@@ -7,37 +7,37 @@ import Missions from '../components/Missions';
 const mockStore = configureStore([]);
 
 describe('test Missions component', () => {
-    it('should render correct snapshot', () => {
-        const mockMissions = [
-            {
-                id: 1,
-                name: 'Mission 1',
-                description: 'This is mission 1',
-                joined: true,
-            },
-            {
-                id: 2,
-                name: 'Mission 2',
-                description: 'This is mission 2',
-                joined: false,
-            },
-        ];
+  it('should render correct snapshot', () => {
+    const mockMissions = [
+      {
+        id: 1,
+        name: 'Mission 1',
+        description: 'This is mission 1',
+        joined: true,
+      },
+      {
+        id: 2,
+        name: 'Mission 2',
+        description: 'This is mission 2',
+        joined: false,
+      },
+    ];
 
-        const store = mockStore({
-            missionsList: {
-                missions: mockMissions,
-                isLoading: false,
-                isError: false,
-            },
-        });
-
-        const tree = renderer
-            .create(
-                <Provider store={store}>
-                    <Missions />
-                </Provider>,
-            )
-            .toJSON();
-        expect(tree).toMatchSnapshot();
+    const store = mockStore({
+      missionsList: {
+        missions: mockMissions,
+        isLoading: false,
+        isError: false,
+      },
     });
+
+    const tree = renderer
+      .create(
+        <Provider store={store}>
+          <Missions />
+        </Provider>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
