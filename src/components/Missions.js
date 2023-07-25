253b1c41492a5e477fa01225ac390780a015da47
Munish <hmunish5@gmail.com>
2023-07-25 09:57:58 +0200

    Add styles for profile page

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,19 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchMissions, joinMission, cancelMission } from '../redux/missions/missionSlice';
+import {
+  fetchMissions,
+  joinMission,
+  cancelMission,
+} from '../redux/missions/missionSlice';
 import './mission.css';
 
 const Missions = () => {
   const dispatch = useDispatch();
-  const { missions: MissionsVar, isLoading, isError } = useSelector((store) => store.missionsList);
+  const {
+    missions: MissionsVar,
+    isLoading,
+    isError,
+  } = useSelector((store) => store.missionsList);
   const storedMissions = JSON.parse(localStorage.getItem('missions')) || [];
   const missions = storedMissions.length > 0 ? storedMissions : MissionsVar;
 
@@ -40,26 +48,40 @@ const Missions = () => {
             </tr>
           </thead>
           <tbody className="t-body">
-
-            { missions.map((mission) => (
-              <tr key={mission.mission_id} className="row">
+            {missions.map((mission) => (
+              <tr key={mission.id} className="row">
                 <td className="name-colum">{mission.name}</td>
                 <td className="description-colum">{mission.description}</td>
                 <td>
-                  {!mission.joined
-                  && <span className="not-member">NOT A MEMBER</span>}
-                  {mission.joined
-                  && <span className="member">Active Member</span>}
+                  {!mission.joined && (
+                    <span className="not-member">NOT A MEMBER</span>
+                  )}
+                  {mission.joined && (
+                    <span className="member">Active Member</span>
+                  )}
                 </td>
                 <td className="text-center">
-                  { mission.joined
-                    && <button type="button" className="btn-cancel" onClick={() => dispatch(cancelMission(mission.id))}>Leave Mission</button>}
-                  {!mission.joined
-                  && <button type="button" className="btn-join" onClick={() => dispatch(joinMission(mission.id))}>Join Mission</button>}
+                  {mission.joined && (
+                    <button
+                      type="button"
+                      className="btn-cancel"
+                      onClick={() => dispatch(cancelMission(mission.id))}
+                    >
+                      Leave Mission
+                    </button>
+                  )}
+                  {!mission.joined && (
+                    <button
+                      type="button"
+                      className="btn-join"
+                      onClick={() => dispatch(joinMission(mission.id))}
+                    >
+                      Join Mission
+                    </button>
+                  )}
                 </td>
               </tr>
             ))}
-
           </tbody>
         </table>
       </div>
